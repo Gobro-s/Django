9f014233ece6af7e7acac6e875c2e7ac8808d0ab
Gobro-s <gobro6366@gmail.com>
2023-04-23 19:58:43 +0900

    secretkey del

--- 07_pjt/mypjt/settings.py
+++ 07_pjt/mypjt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i%7c-(2d_xhs8w-me9wh@&)6tm*!lt*d@)m1*p4adaq^#^c-$k"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
